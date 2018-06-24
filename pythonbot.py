57649a2fa8fa623621360b422dee93f8f969c005
Manank Patni <manank321@gmail.com>
2018-06-24 17:27:48 +0530

    Added Logging

--- pythonbot.py
+++ pythonbot.py
@@ -1,9 +1,10 @@
 import requests
 import json
+import logging
 
 def send_reply():
 	msg=d['result'][-1]['message']['text']
-	print('The message recieved is '+msg)
+	log.info('The message recieved is '+msg)
 	if msg=='/hello':
 		return 'Hey There'
 	elif msg=='/gmail':
@@ -12,45 +13,49 @@ def send_reply():
 		return 'Man-Jain'
 	elif msg=='/contact':
 		return '@Man_Jain'
+	else:
+		return 'Sorry Could Not Understand You'
 
 def file_read():
 	with open('updateid.tdt') as f:
 		prev_id=f.read()
-		print('Previous ID: '+prev_id)
+		log.info('Previous ID: '+prev_id)
 		return prev_id
 
 def main():
 	update_id=''
 	global d
 	d={}
-	bot_key=''
+	bot_key='560195331:AAGDggiM9L8R6M-OL4f3g0wMAf9o_WaMLiI'
 	url='https://api.telegram.org/bot'+bot_key+'/getUpdates'
-	print('The URL is: '+url)
+	log.info('The URL is: '+url)
 
 	while True:
 		prev_id=file_read()
-		print('Requesting url...')
+		log.info('Requesting url...')
 		req=requests.get(url)
 		d=req.json()
 		latest_id=str(d["result"][-1]['update_id'])
-		print('Latest ID: '+latest_id)
+		log.info('Latest ID: '+latest_id)
 
 
 		if prev_id==latest_id:
-			print('Waiting for users message...')
+			log.info('Waiting for users message...')
 			continue
 		else:
 			reply_text=send_reply()
 			chat_id=d['result'][-1]['message']['from']['id']
-			print('Chat ID: '+str(chat_id))
+			log.info('Chat ID: '+str(chat_id))
 			send_url='https://api.telegram.org/bot'+bot_key+'/sendMessage?chat_id='+str(chat_id)+'&text='+str(reply_text)
-			print('Sending Message '+reply_text)
+			log.info('Sending Message '+reply_text)
 			send_req=requests.get(send_url)
-			print(send_req)
+			log.info(send_req)
 			update_id=str(d['result'][-1]['update_id'])
-			print('Writing id to file: '+str(update_id))
+			log.info('Writing id to file: '+str(update_id))
 			with open('updateid.tdt','w') as fw:
 				fw.write(update_id)
 
 if __name__=='__main__':
+	logging.basicConfig(level=logging.INFO)
+	log=logging.getLogger(__name__)
 	main()
